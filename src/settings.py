ec42b7452108129b15ef509597d54d76700342fa
Egor <serochkin.egor@icloud.com>
2026-03-07 19:14:48 +0300

    Fix

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql+asyncpg://postgres:password@db:5432/postgres"
+    database_url: str = "postgresql+asyncpg://postgres:password@localhost:5432/postgres"
     secret_key: str = "secret"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
